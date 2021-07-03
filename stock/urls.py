934e0f91466da91ebd76ea5520e4759df782e6fe
Sergei Petrov <serega990306@mail.ru>
2021-07-03 15:56:19 +0300

    Urls update

--- stock/urls.py
+++ stock/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from .views import StockView, SectorView, StockHistoryView, SectorStockView
 
 router = routers.SimpleRouter()
-router.register(r'sector', SectorView)
+router.register(r'sectors', SectorView)
 router.register(r'sector_stock', SectorStockView)
 router.register(r'stock', StockView)
 router.register(r'stock_hist', StockHistoryView)
